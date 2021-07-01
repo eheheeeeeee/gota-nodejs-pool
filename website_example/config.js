c0f7222676ee7166c168a742cfcf3cb617255c91
Dev <>
2021-07-01 19:53:04 +0200

    Initial release

---
+++ website_example/config.js
@@ -0,0 +1,18 @@
+var api = "https://your-api.pool.com:4010";
+var apiMerged = "";
+let parentCoin = ""
+
+var email = "";
+var telegram = "";
+var discord = "";
+var facebook = "";
+
+var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
+
+var blockchainExplorer = "https://explorer.gotanetwork.org/{symbol}/block/{id}";
+var blockchainExplorerMerged = "https://explorer.gotanetwork.org/?hash={id}#block";
+var transactionExplorer = "https://explorer.gotanetwork.org/{symbol}/transaction/{id}";
+var transactionExplorerMerged = "https://explorer.gotanetwork.org/?hash={id}#transaction";
+
+var themeCss = "themes/default.css";
+var defaultLang = 'en';
